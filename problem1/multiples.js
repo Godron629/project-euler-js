808a1ec6345549fe0301111c57b92993205a2fad
Gideon Richter <gideonrich@hotmail.ca>
2016-12-22 10:56:24 -0700

    Add problem1 answer

---
+++ problem1/multiples.js
@@ -0,0 +1,23 @@
+/*a and b are multiples to test and 
+c is the upper limit of returned values*/
+function multiples(a, b, c) {
+	var multipleList = [];
+
+	for(var i = 1; i <= c; i++) {
+		if(i % a == 0 || i % b == 0) {
+			multipleList.push(i);
+		}
+	}
+	return multipleList;
+}
+
+function multiplesSum(a, b, c) {
+	var multiplesSum = 0;
+
+	for(var i = 1; i <= c; i++) {
+		if(i % a == 0 || i % b == 0) {
+			multiplesSum += i;
+		}
+	}
+	return multiplesSum;
+}
